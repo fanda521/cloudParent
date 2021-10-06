4e6d89857109fadff6422d68c49027491db1477e
fanda521 <1056015243@qq.com>
2021-10-06 15:47:56 +0800

    初始化

---
+++ cloud-provider-hystrix-payment8101/src/test/java/com/wang/cloudproviderhystrixpayment8102/CloudProviderHystrixPayment8102ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wang.cloudproviderhystrixpayment8102;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudProviderHystrixPayment8102ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
